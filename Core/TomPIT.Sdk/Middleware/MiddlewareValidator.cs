e5ed938f26a125d82460352296bf76209998fd24
Matija Koželj <matija.kozelj@tompit.net>
2022-08-10 09:19:29 +0200

    [WIP] Temporary antiforgery shutdown

--- Core/TomPIT.Sdk/Middleware/MiddlewareValidator.cs
+++ Core/TomPIT.Sdk/Middleware/MiddlewareValidator.cs
@@ -88,7 +88,7 @@ namespace TomPIT.Middleware
 
             try 
             {
-
+                return;
                 Trace($"Validating antiforgery {sw.ElapsedMilliseconds}");
                 if (AsyncUtils.RunSync(() => service.IsRequestValidAsync(Shell.HttpContext)))
                 {
