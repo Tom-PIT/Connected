6ab5d873afa12288b4e6297e57d4f8d114acfc93
Matija Koželj <matija.kozelj@tompit.net>
2022-08-19 10:39:38 +0200

    Add translation removal to StringResource

--- Development/TomPIT.Templates.Application/Resources/StringResource.cs
+++ Development/TomPIT.Templates.Application/Resources/StringResource.cs
@@ -10,52 +10,48 @@ using TomPIT.MicroServices.Design;
 
 namespace TomPIT.MicroServices.Resources
 {
-	[Create(DesignUtils.String, nameof(Key))]
-	public class StringResource : ConfigurationElement, IStringResource
-	{
-		private ListItems<IStringTranslation> _translations = null;
+    [Create(DesignUtils.String, nameof(Key))]
+    public class StringResource : ConfigurationElement, IStringResource
+    {
+        private ListItems<IStringTranslation> _translations = null;
 
-		[Required]
-		[InvalidateEnvironment(EnvironmentSection.Designer | EnvironmentSection.Explorer)]
-		[PropertyCategory(PropertyCategoryAttribute.CategoryDesign)]
-		public string Key { get; set; }
-		[PropertyCategory(PropertyCategoryAttribute.CategoryAppearance)]
-		[PropertyEditor(PropertyEditorAttribute.TextArea)]
-		public string DefaultValue { get; set; }
-		[PropertyCategory(PropertyCategoryAttribute.CategoryBehavior)]
-		public bool IsLocalizable { get; set; } = true;
+        [Required]
+        [InvalidateEnvironment(EnvironmentSection.Designer | EnvironmentSection.Explorer)]
+        [PropertyCategory(PropertyCategoryAttribute.CategoryDesign)]
+        public string Key { get; set; }
+        [PropertyCategory(PropertyCategoryAttribute.CategoryAppearance)]
+        [PropertyEditor(PropertyEditorAttribute.TextArea)]
+        public string DefaultValue { get; set; }
+        [PropertyCategory(PropertyCategoryAttribute.CategoryBehavior)]
+        public bool IsLocalizable { get; set; } = true;
 
-		[Browsable(false)]
-		public ListItems<IStringTranslation> Translations
-		{
-			get
-			{
-				if (_translations == null)
-					_translations = new ListItems<IStringTranslation> { Parent = this };
+        [Browsable(false)]
+        public ListItems<IStringTranslation> Translations => _translations ??= new ListItems<IStringTranslation> { Parent = this };
 
-				return _translations;
-			}
-		}
+        public override string ToString()
+        {
+            return string.IsNullOrWhiteSpace(Key)
+                ? base.ToString()
+                : Key;
+        }
 
-		public override string ToString()
-		{
-			return string.IsNullOrWhiteSpace(Key)
-				? base.ToString()
-				: Key;
-		}
-
-		public void UpdateTranslation(int lcid, string value)
-		{
-			if (Translations.FirstOrDefault(f => f.Lcid == lcid) is StringTranslation existing)
-				existing.Value = value;
-			else
-			{
-				Translations.Add(new StringTranslation
-				{
-					Lcid = lcid,
-					Value = value
-				});
-			}
-		}
-	}
+        public void UpdateTranslation(int lcid, string value)
+        {
+            if (Translations.FirstOrDefault(f => f.Lcid == lcid) is StringTranslation existing)
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    Translations.Remove(existing);
+                else
+                    existing.Value = value;
+            }
+            else
+            {
+                Translations.Add(new StringTranslation
+                {
+                    Lcid = lcid,
+                    Value = value
+                });
+            }
+        }
+    }
 }
