0f34850f8b7f38eae3dea80bb56dd508de6b0ff2
Matija Koželj <matija.kozelj@tompit.net>
2022-11-17 19:05:00 +0100

    Add missing Saas client files Add Saas to build solution Add Saas to project references

---
+++ Saas/Clients/Connected.SaaS.Clients/Connected.SaaS.Clients/Authentication/IRestAuthenticationProvider.cs
@@ -0,0 +1,15 @@
+﻿using Connected.SaaS.Clients.Rest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connected.SaaS.Clients.Authentication
+{
+    public interface IRestAuthenticationProvider
+    {
+        void Apply(HttpRequestMessage message);
+    }
+}
