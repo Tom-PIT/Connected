a365c1e06730d450f41affa847b49cded0a8fa5c
Matija Koželj <matija.kozelj@tompit.net>
2022-12-01 13:28:03 +0100

    Add null check to message data load

--- Sys/TomPIT.Sys/Model/Cdn/MessageRecipientsModel.cs
+++ Sys/TomPIT.Sys/Model/Cdn/MessageRecipientsModel.cs
@@ -6,99 +6,109 @@ using TomPIT.SysDb.Messaging;
 
 namespace TomPIT.Sys.Model.Cdn
 {
-	internal class MessageRecipientsModel : IdentityRepository<IRecipient, string>
-	{
-		public MessageRecipientsModel(IMemoryCache container) : base(container, "messagerecipient")
-		{
-		}
+    internal class MessageRecipientsModel : IdentityRepository<IRecipient, string>
+    {
+        public MessageRecipientsModel(IMemoryCache container) : base(container, "messagerecipient")
+        {
+        }
 
-		public IRecipient Select(Guid message, string connection)
-		{
-			return Get(GenerateKey(message, connection));
-		}
+        public IRecipient Select(Guid message, string connection)
+        {
+            return Get(GenerateKey(message, connection));
+        }
 
-		public void Load(Guid message)
-		{
-			var m = DataModel.Messages.Select(message);
-			var subscribers = DataModel.MessageSubscribers.Query(m.Topic);
+        public void Load(Guid message)
+        {
+            var m = DataModel.Messages.Select(message);
 
-			foreach (var subscriber in subscribers)
-			{
-				Set(GenerateKey(message, subscriber.Connection), new MessageRecipient
-				{
-					Connection = subscriber.Connection,
-					Content = m.Text,
-					Id = Increment(),
-					Message = message,
-					NextVisible = DateTime.UtcNow,
-					RetryCount = 10,
-					Topic = m.Topic
-				});
-			}
-		}
+            if (m is null)
+            {
+                /*
+				 * No message to load subscribers for.
+				 * TODO: Log message for posteriority.
+                 */
+                 return;
+            }
 
-		public void Remove(IRecipient recipient)
-		{
-			Remove(GenerateKey(recipient.Message, recipient.Connection));
-		}
-		public void Delete(Guid message)
-		{
-			var ds = Where(f => f.Message == message);
+            var subscribers = DataModel.MessageSubscribers.Query(m.Topic);
 
-			foreach (var i in ds)
-				Remove(GenerateKey(i.Message, i.Connection));
-		}
+            foreach (var subscriber in subscribers)
+            {
+                Set(GenerateKey(message, subscriber.Connection), new MessageRecipient
+                {
+                    Connection = subscriber.Connection,
+                    Content = m.Text,
+                    Id = Increment(),
+                    Message = message,
+                    NextVisible = DateTime.UtcNow,
+                    RetryCount = 10,
+                    Topic = m.Topic
+                });
+            }
+        }
 
-		public void Delete(string topic, string connection)
-		{
-			var t = DataModel.MessageTopics.Ensure(topic);
-			var s = DataModel.MessageSubscribers.Select(topic, connection);
+        public void Remove(IRecipient recipient)
+        {
+            Remove(GenerateKey(recipient.Message, recipient.Connection));
+        }
+        public void Delete(Guid message)
+        {
+            var ds = Where(f => f.Message == message);
 
-			if (s == null)
-				return;
+            foreach (var i in ds)
+                Remove(GenerateKey(i.Message, i.Connection));
+        }
 
-			var ds = Where(f => string.Compare(f.Topic, topic, true) == 0
-				 && string.Compare(f.Connection, connection, true) == 0);
+        public void Delete(string topic, string connection)
+        {
+            var t = DataModel.MessageTopics.Ensure(topic);
+            var s = DataModel.MessageSubscribers.Select(topic, connection);
 
-			foreach (var i in ds)
-				Remove(GenerateKey(i.Message, i.Connection));
-		}
+            if (s == null)
+                return;
 
-		public ImmutableList<IRecipient> Query(Guid message)
-		{
-			return Where(f => f.Message == message);
-		}
+            var ds = Where(f => string.Compare(f.Topic, topic, true) == 0
+                 && string.Compare(f.Connection, connection, true) == 0);
 
-		public ImmutableList<IRecipient> QueryScheduled()
-		{
-			var ds = Where(f => f.NextVisible < DateTime.UtcNow);
+            foreach (var i in ds)
+                Remove(GenerateKey(i.Message, i.Connection));
+        }
 
-			foreach (var i in ds)
-			{
-				var m = DataModel.Messages.Select(i.Message);
+        public ImmutableList<IRecipient> Query(Guid message)
+        {
+            return Where(f => f.Message == message);
+        }
 
-				if (m is null || m.Expire < DateTime.UtcNow)
-				{
-					DataModel.Messages.Delete(m.Token);
-					continue;
-				}
+        public ImmutableList<IRecipient> QueryScheduled()
+        {
+            var ds = Where(f => f.NextVisible < DateTime.UtcNow);
 
-				if (i.RetryCount > 10)
-				{
-					Remove(i);
-					continue;
-				}
+            foreach (var i in ds)
+            {
+                var m = DataModel.Messages.Select(i.Message);
 
-				i.RetryCount++;
-				i.NextVisible = DateTime.UtcNow.Add(m.RetryInterval);
-			}
+                if (m is null || m.Expire < DateTime.UtcNow)
+                {
+                    DataModel.Messages.Delete(m.Token);
+                    continue;
+                }
 
-			return ds;
-		}
+                if (i.RetryCount > 10)
+                {
+                    Remove(i);
+                    continue;
+                }
 
-		public void Delete(Guid message, string connection)
-		{
-			Remove(GenerateKey(message, connection));
-		}
-	}
+                i.RetryCount++;
+                i.NextVisible = DateTime.UtcNow.Add(m.RetryInterval);
+            }
+
+            return ds;
+        }
+
+        public void Delete(Guid message, string connection)
+        {
+            Remove(GenerateKey(message, connection));
+        }
+    }
 }
