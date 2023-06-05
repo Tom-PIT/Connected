fdc7b534627f456d118a965b799bae6b6ecd0146
Matija Koželj <matija.kozelj@tompit.net>
2023-06-05 10:01:35 +0200

    Modify event logger to write to file instead due to access issues

--- Core/TomPIT.Core/Diagnostics/WindowsEventLog.cs
+++ Core/TomPIT.Core/Diagnostics/WindowsEventLog.cs
@@ -1,17 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.Text;
 
 namespace TomPIT.Diagnostics
 {
    public static class EventLog
    {
+      private static object WriteLock = new object();
       private static void Write(string message, EventLogEntryType type)
       {
          try
          {
-            using System.Diagnostics.EventLog eventLog = new("Application");
-            eventLog.Source = "TomPIT.Sys";
-            eventLog.WriteEntry(message, type);
+            lock (WriteLock)
+            {
+               //   using System.Diagnostics.EventLog eventLog = new("Application");
+               //   eventLog.Source = "TomPIT.Sys";
+               //   eventLog.WriteEntry(message, type);
+               var fileName = $"dump_{DateTimeOffset.UtcNow.Year}_{DateTimeOffset.UtcNow.Month}_{DateTimeOffset.UtcNow.Day}.txt";
+               var directory = Path.Combine("logs");
+               var path = Path.Combine(directory, fileName);
+
+               if (!Directory.Exists(directory))
+                  Directory.CreateDirectory(directory);
+
+               File.AppendAllLines(path, new[] { message });
+            }
          }
          catch
          {
--- Sys/TomPIT.Sys/Diagnostics/EventLog.cs
+++ Sys/TomPIT.Sys/Diagnostics/EventLog.cs
@@ -1,37 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text;
 
-namespace TomPIT.Sys.Diagnostics
+namespace TomPIT.Diagnostics
 {
-    public static class EventLog
-    {
-        private static void Write(string message, EventLogEntryType type)
-        {
-            try
+   public static class EventLog
+   {
+      private static object WriteLock = new object();
+      private static void Write(string message, EventLogEntryType type)
+      {
+         try
+         {
+            lock (WriteLock)
             {
-                using System.Diagnostics.EventLog eventLog = new("Application");
-                eventLog.Source = "TomPIT.Sys";
-                eventLog.WriteEntry(message, type);
+               //   using System.Diagnostics.EventLog eventLog = new("Application");
+               //   eventLog.Source = "TomPIT.Sys";
+               //   eventLog.WriteEntry(message, type);
+               var fileName = $"dump_{DateTimeOffset.UtcNow.Year}_{DateTimeOffset.UtcNow.Month}_{DateTimeOffset.UtcNow.Day}.txt";
+               var directory = Path.Combine("logs");
+               var path = Path.Combine(directory, fileName);
+
+               if (!Directory.Exists(directory))
+                  Directory.CreateDirectory(directory);
+
+               File.AppendAllLines(path, new[] { message });
             }
-            catch
-            {
-                //Oh well...
-            }
-        }
+         }
+         catch
+         {
+            //Oh well...
+         }
+      }
 
-        private static void WindowsCheck(Action e)
-        {
-            if (!OperatingSystem.IsWindows())
-                return;
+      private static void WindowsCheck(Action e)
+      {
+         if (!OperatingSystem.IsWindows())
+            return;
 
-            e();
-        }
+         e();
+      }
 
-        public static void WriteInfo(string message) => WindowsCheck(() => Write(message, EventLogEntryType.Information));
-        public static void WriteError(string message) => WindowsCheck(() => Write(message, EventLogEntryType.Error));
-        public static void WriteWarning(string message) => WindowsCheck(() => Write(message, EventLogEntryType.Warning));
-    }
+      public static void WriteInfo(string message) => WindowsCheck(() => Write(message, EventLogEntryType.Information));
+      public static void WriteError(string message) => WindowsCheck(() => Write(message, EventLogEntryType.Error));
+      public static void WriteWarning(string message) => WindowsCheck(() => Write(message, EventLogEntryType.Warning));
+   }
 }
