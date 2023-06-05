59bc602efddf8812913c7a6cd9da4d3bb5dc94db
Matija Koželj <matija.kozelj@tompit.net>
2023-06-05 10:11:59 +0200

    Fix event log reference errors

--- Sys/TomPIT.Sys/Exceptions/ExceptionHubFilter.cs
+++ Sys/TomPIT.Sys/Exceptions/ExceptionHubFilter.cs
@@ -3,7 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using TomPIT.Sys.Diagnostics;
+
+using TomPIT.Diagnostics;
 
 namespace TomPIT.Sys.Exceptions
 {
--- Sys/TomPIT.Sys/Startup.cs
+++ Sys/TomPIT.Sys/Startup.cs
@@ -24,151 +24,151 @@ using static TomPIT.HttpExtensions;
 
 namespace TomPIT.Sys
 {
-	public class Startup
-	{
-		public Startup(IConfiguration configuration)
-		{
-			Configuration = configuration;
-		}
+   public class Startup
+   {
+      public Startup(IConfiguration configuration)
+      {
+         Configuration = configuration;
+      }
 
-		public IConfiguration Configuration { get; }
+      public IConfiguration Configuration { get; }
 
-		public void ConfigureServices(IServiceCollection services)
-		{
-			Shell.RegisterConfigurationType(typeof(ServerSys));
+      public void ConfigureServices(IServiceCollection services)
+      {
+         Shell.RegisterConfigurationType(typeof(ServerSys));
 
-			services.AddMvc(o =>
-			{
-				o.EnableEndpointRouting = false;
-			}).AddNewtonsoftJson();
+         services.AddMvc(o =>
+         {
+            o.EnableEndpointRouting = false;
+         }).AddNewtonsoftJson();
 
-			services.AddAuthentication(options =>
-			{
-				options.DefaultAuthenticateScheme = "TomPIT";
-				options.DefaultChallengeScheme = "TomPIT";
-				options.DefaultScheme = "TomPIT";
-			}).AddTomPITAuthentication("TomPIT", "Tom PIT", o =>
-			{
+         services.AddAuthentication(options =>
+         {
+            options.DefaultAuthenticateScheme = "TomPIT";
+            options.DefaultChallengeScheme = "TomPIT";
+            options.DefaultScheme = "TomPIT";
+         }).AddTomPITAuthentication("TomPIT", "Tom PIT", o =>
+         {
 
-			});
+         });
 
-			services.AddAuthorization();
+         services.AddAuthorization();
 
-			services.AddSignalR(o =>
-			{
-				o.EnableDetailedErrors = true;
-				o.AddFilter<ExceptionHubFilter>();
-			});
+         services.AddSignalR(o =>
+         {
+            o.EnableDetailedErrors = true;
+            o.AddFilter<ExceptionHubFilter>();
+         });
 
-			services.AddSingleton<ExceptionHubFilter>();
+         services.AddSingleton<ExceptionHubFilter>();
 
-			RegisterTasks(services);
+         RegisterTasks(services);
 
-			services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-		}
+         services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+      }
 
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-				app.UseTomPITExceptionMiddleware();
-			}
-			else
-			{
-				app.UseTomPITExceptionMiddleware();
-				//app.UseExceptionHandler();
-			}
+      public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+      {
+         if (env.IsDevelopment())
+         {
+            app.UseDeveloperExceptionPage();
+            app.UseTomPITExceptionMiddleware();
+         }
+         else
+         {
+            app.UseTomPITExceptionMiddleware();
+            //app.UseExceptionHandler();
+         }
 
-			RegisterTraceService(app);
+         RegisterTraceService(app);
 
-			app.UseStaticFiles();
-			app.UseRouting();
-			app.UseAuthentication();
-			app.UseAuthorization();
+         app.UseStaticFiles();
+         app.UseRouting();
+         app.UseAuthentication();
+         app.UseAuthorization();
 
-			app.UseEndpoints(routes =>
-			{
-				routes.MapHub<CacheHub>("/caching");
-				routes.MapHub<IoTHub>("/iot");
-				routes.MapHub<BigDataHub>("/bigdata");
-				routes.MapHub<DataCacheHub>("/datacaching");
-				routes.MapHub<TraceHub>("hubs/tracing");
-			});
+         app.UseEndpoints(routes =>
+         {
+            routes.MapHub<CacheHub>("/caching");
+            routes.MapHub<IoTHub>("/iot");
+            routes.MapHub<BigDataHub>("/bigdata");
+            routes.MapHub<DataCacheHub>("/datacaching");
+            routes.MapHub<TraceHub>("hubs/tracing");
+         });
 
-			app.UseMvc(routes =>
-			{
-				routes.MapRoute("default", "{controller}/{action}");
-			});
+         app.UseMvc(routes =>
+         {
+            routes.MapRoute("default", "{controller}/{action}");
+         });
 
-			ServerConfiguration.Initialize(app);
-			Shell.Configure(app);
-			DataModel.Initialized = true;
+         ServerConfiguration.Initialize(app);
+         Shell.Configure(app);
+         DataModel.Initialized = true;
 
 
-			Diagnostics.EventLog.WriteInfo("Sys started");
-		}
+         TomPIT.Diagnostics.EventLog.WriteInfo("Sys started");
+      }
 
-		private void RegisterTraceService(IApplicationBuilder app)
-		{
-			var traceHubContext = app.ApplicationServices.GetRequiredService<IHubContext<TraceHub>>();
+      private void RegisterTraceService(IApplicationBuilder app)
+      {
+         var traceHubContext = app.ApplicationServices.GetRequiredService<IHubContext<TraceHub>>();
 
-			var traceService = app.ApplicationServices.GetService<ITraceService>();
+         var traceService = app.ApplicationServices.GetService<ITraceService>();
 
-			traceService.TraceReceived += async (s, e) => await TraceHub.Trace(traceHubContext, e);
+         traceService.TraceReceived += async (s, e) => await TraceHub.Trace(traceHubContext, e);
 
-			traceService.AddEndpoint("TomPIT.Sys.Diagnostics", "IncomingRequest");
-			traceService.AddEndpoint("TomPIT.Sys.Diagnostics", "LongLastingRequest");
-			traceService.AddEndpoint("TomPIT.Sys.Diagnostics", "UserControllerRequest");
+         traceService.AddEndpoint("TomPIT.Sys.Diagnostics", "IncomingRequest");
+         traceService.AddEndpoint("TomPIT.Sys.Diagnostics", "LongLastingRequest");
+         traceService.AddEndpoint("TomPIT.Sys.Diagnostics", "UserControllerRequest");
 
-			app.Use(async (context, next) =>
-			{
-				context.Request.EnableBuffering();
+         app.Use(async (context, next) =>
+         {
+            context.Request.EnableBuffering();
 
-				var path = context.Request.Path;
-				var stopwatch = Stopwatch.StartNew();
+            var path = context.Request.Path;
+            var stopwatch = Stopwatch.StartNew();
 
-				var body = context.Request.Body.ToJObject();
+            var body = context.Request.Body.ToJObject();
 
-				traceService.Trace("TomPIT.Sys.Diagnostics", "IncomingRequest", $"{path} {Serializer.Serialize(body)} {Serializer.Serialize(context.Request.Query)}");
+            traceService.Trace("TomPIT.Sys.Diagnostics", "IncomingRequest", $"{path} {Serializer.Serialize(body)} {Serializer.Serialize(context.Request.Query)}");
 
-				if (path.HasValue && path.Value.ToLower().Contains("user"))
-				{
-					var sourceData = new
-					{
-						SourceIp = context.Connection?.RemoteIpAddress?.ToString(),
-						SourcePort = context.Connection?.RemotePort.ToString(),
-						User = context.User?.Identity?.Name,
-						UserAuthenticated = context.User?.Identity?.IsAuthenticated
-					};
+            if (path.HasValue && path.Value.ToLower().Contains("user"))
+            {
+               var sourceData = new
+               {
+                  SourceIp = context.Connection?.RemoteIpAddress?.ToString(),
+                  SourcePort = context.Connection?.RemotePort.ToString(),
+                  User = context.User?.Identity?.Name,
+                  UserAuthenticated = context.User?.Identity?.IsAuthenticated
+               };
 
-					var userRequestMessage = $"Path: {path} Payload: {Serializer.Serialize(body)}, QueryParams: {Serializer.Serialize(context.Request.Query)}, Source: {Serializer.Serialize(sourceData)}";
+               var userRequestMessage = $"Path: {path} Payload: {Serializer.Serialize(body)}, QueryParams: {Serializer.Serialize(context.Request.Query)}, Source: {Serializer.Serialize(sourceData)}";
 
-					traceService.Trace("TomPIT.Sys.Diagnostics", "UserControllerRequest", userRequestMessage);
-				}
+               traceService.Trace("TomPIT.Sys.Diagnostics", "UserControllerRequest", userRequestMessage);
+            }
 
-				if (next is not null && !context.Response.HasStarted)
-					await next();
+            if (next is not null && !context.Response.HasStarted)
+               await next();
 
-				if (stopwatch.ElapsedMilliseconds > 2000)
-					traceService.Trace("TomPIT.Sys.Diagnostics", "LongLastingRequest", path);
-			});
+            if (stopwatch.ElapsedMilliseconds > 2000)
+               traceService.Trace("TomPIT.Sys.Diagnostics", "LongLastingRequest", path);
+         });
 
-		}
+      }
 
-		private void RegisterTasks(IServiceCollection services)
-		{
-			services.AddSingleton<ITraceService, TraceService>();
-			services.AddSingleton<IHostedService, MessageDispatcher>();
-			services.AddSingleton<IHostedService, MessageDisposer>();
-			services.AddSingleton<IHostedService, Scheduler>();
-			services.AddSingleton<IHostedService, Preloader>();
-			services.AddSingleton<IHostedService, QueuePersistence>();
-			services.AddSingleton<IHostedService, EventsPersistence>();
-			services.AddSingleton<IHostedService, PrintingPersistence>();
-			services.AddSingleton<IHostedService, PrintingSpoolerPersistence>();
-			services.AddSingleton<IHostedService, BigDataBufferPersistence>();
-			services.AddHostedService<HealthMonitoringHostedService>();
-		}
-	}
+      private void RegisterTasks(IServiceCollection services)
+      {
+         services.AddSingleton<ITraceService, TraceService>();
+         services.AddSingleton<IHostedService, MessageDispatcher>();
+         services.AddSingleton<IHostedService, MessageDisposer>();
+         services.AddSingleton<IHostedService, Scheduler>();
+         services.AddSingleton<IHostedService, Preloader>();
+         services.AddSingleton<IHostedService, QueuePersistence>();
+         services.AddSingleton<IHostedService, EventsPersistence>();
+         services.AddSingleton<IHostedService, PrintingPersistence>();
+         services.AddSingleton<IHostedService, PrintingSpoolerPersistence>();
+         services.AddSingleton<IHostedService, BigDataBufferPersistence>();
+         services.AddHostedService<HealthMonitoringHostedService>();
+      }
+   }
 }
