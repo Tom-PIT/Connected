d658c9de828fdf882b8fcb2ce0cce5a54f185ece
Matija Koželj <matija.kozelj@tompit.net>
2023-06-20 10:47:53 +0200

    Add signalR connection delay until server has started

--- Runtime/TomPIT.Runtime/Connectivity/ClientConnection.cs
+++ Runtime/TomPIT.Runtime/Connectivity/ClientConnection.cs
@@ -1,110 +1,117 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Http.Connections.Client;
 using Microsoft.AspNetCore.SignalR.Client;
 
 namespace TomPIT.Connectivity
 {
-	public abstract class ClientConnection : TenantObject, IDisposable
-	{
-		public event EventHandler Connected;
-		protected ClientConnection()
-		{
-		}
+    public abstract class ClientConnection : TenantObject, IDisposable
+    {
+        public event EventHandler Connected;
+        protected ClientConnection()
+        {
+        }
 
-		protected ClientConnection(ITenant tenant) : base(tenant)
-		{
-		}
+        protected ClientConnection(ITenant tenant) : base(tenant)
+        {
+        }
 
-		protected CancellationTokenSource Cancel { get; } = new CancellationTokenSource();
-		protected HubConnection Hub { get; private set; }
-		protected abstract string HubName { get; }
-		protected abstract string Url { get; }
-		protected virtual Action<HttpConnectionOptions> Options => null;
-		public async void Connect()
-		{
-			if (Hub != null)
-				Disconnect();
+        protected CancellationTokenSource Cancel { get; } = new CancellationTokenSource();
+        protected HubConnection Hub { get; private set; }
+        protected abstract string HubName { get; }
+        protected abstract string Url { get; }
+        protected virtual Action<HttpConnectionOptions> Options => null;
+        public async void Connect()
+        {
+            if (Hub != null)
+                Disconnect();
 
-			Hub = Options == null
-				? new HubConnectionBuilder().WithUrl(Url).Build()
-				: new HubConnectionBuilder().WithUrl(Url, Options).Build();
+            Hub = Options == null
+                ? new HubConnectionBuilder().WithUrl(Url).Build()
+                : new HubConnectionBuilder().WithUrl(Url, Options).Build();
 
-			Initialize();
+            Initialize();
 
-			Hub.Closed += OnClosed;
+            Hub.Closed += OnClosed;
 
-			await ConnectAsync();
-		}
+            await ConnectAsync();
+        }
 
-		private Task OnClosed(Exception arg)
-		{
-			Cancel.Cancel();
+        private Task OnClosed(Exception arg)
+        {
+            Cancel.Cancel();
 
-			Connect();
+            Connect();
 
-			return Task.CompletedTask;
-		}
+            return Task.CompletedTask;
+        }
 
-		protected virtual void Initialize()
-		{
-		}
+        protected virtual void Initialize()
+        {
+        }
 
-		public void Disconnect()
-		{
-			if (Hub == null)
-				return;
+        public void Disconnect()
+        {
+            if (Hub == null)
+                return;
 
-			Task.FromResult(Hub.DisposeAsync()).GetAwaiter().GetResult();
+            Task.FromResult(Hub.DisposeAsync()).GetAwaiter().GetResult();
 
-			Hub = null;
-		}
+            Hub = null;
+        }
 
-		private async Task<bool> ConnectAsync()
-		{
-			while (true)
-			{
-				try
-				{
-					if (Hub.State != HubConnectionState.Disconnected)
-						return false;
+        private async Task<bool> ConnectAsync()
+        {
+            while (true)
+            {
+                if (Instance.State != InstanceState.Running)
+                {
+                    await Task.Delay(50);
+                    continue;
+                }
 
-					await Hub.StartAsync();
+                try
+                {
+                    if (Hub.State != HubConnectionState.Disconnected)
+                        return false;
 
-					Connected?.Invoke(this, EventArgs.Empty);
-					OnConnected();
+                    await Hub.StartAsync();
 
-					return true;
-				}
-				catch (ObjectDisposedException)
-				{
-					return false;
-				}
-				catch
-				{
-					await Task.Delay(2000);
-				}
-			}
-		}
+                    Connected?.Invoke(this, EventArgs.Empty);
+                    OnConnected();
 
-		protected virtual void OnConnected()
-		{
+                    return true;
+                }
+                catch (ObjectDisposedException)
+                {
+                    return false;
+                }
+                catch
+                {
+                    await Task.Delay(2000);
+                }
+            }
+        }
 
-		}
+        protected virtual void OnConnected()
+        {
 
-		public void Dispose()
-		{
-			Dispose(true);
-		}
+        }
 
-		protected virtual void Dispose(bool disposing)
-		{
-			GC.SuppressFinalize(this);
+        public void Dispose()
+        {
+            Dispose(true);
+        }
 
-			if (disposing)
-				Disconnect();
-		}
-		public bool IsConnected => Hub != null && Hub.State == HubConnectionState.Connected;
-	}
+        protected virtual void Dispose(bool disposing)
+        {
+            GC.SuppressFinalize(this);
+
+            if (disposing)
+                Disconnect();
+        }
+        public bool IsConnected => Hub != null && Hub.State == HubConnectionState.Connected;
+    }
 }
