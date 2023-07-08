0fc00063000b64dfcddff08b68b6e9d04d78a560
Tom Pipinic <tom@tompit.net>
2023-07-08 10:02:03 +0200

    Removed unused parameter locally_hosted

--- Sys/TomPIT.SysDb/Environment/IEnvironmentHandler.cs
+++ Sys/TomPIT.SysDb/Environment/IEnvironmentHandler.cs
@@ -4,34 +4,34 @@ using TomPIT.Environment;
 
 namespace TomPIT.SysDb.Environment
 {
-	public interface IEnvironmentHandler
-	{
-		IClientHandler Clients { get; }
+    public interface IEnvironmentHandler
+    {
+        IClientHandler Clients { get; }
 
-		/*
+        /*
 		 * Instance endpoints
 		 */
-		void InsertInstanceEndpoint(Guid token, InstanceFeatures features, string name, string url, string reverseProxyUrl, InstanceStatus status, InstanceVerbs verbs);
-		void UpdateInstanceEndpoint(IInstanceEndpoint item, InstanceFeatures features, string name, string url, string reverseProxyUrl, InstanceStatus status, InstanceVerbs verbs, bool locallyHosted = false);
-		void DeleteInstanceEndpoint(IInstanceEndpoint item);
+        void InsertInstanceEndpoint(Guid token, InstanceFeatures features, string name, string url, string reverseProxyUrl, InstanceStatus status, InstanceVerbs verbs);
+        void UpdateInstanceEndpoint(IInstanceEndpoint item, InstanceFeatures features, string name, string url, string reverseProxyUrl, InstanceStatus status, InstanceVerbs verbs);
+        void DeleteInstanceEndpoint(IInstanceEndpoint item);
 
-		IInstanceEndpoint SelectInstanceEndpoint(Guid token);
-		List<IInstanceEndpoint> QueryInstanceEndpoints();
-		/*
+        IInstanceEndpoint SelectInstanceEndpoint(Guid token);
+        List<IInstanceEndpoint> QueryInstanceEndpoints();
+        /*
 		 * Resource groups
 		 */
-		void InsertResourceGroup(Guid token, string name, Guid storageProvider, string connectionString);
-		void UpdateResourceGroup(IResourceGroup item, string name, Guid storageProvider, string connectionString);
-		void DeleteResourceGroup(IResourceGroup item);
+        void InsertResourceGroup(Guid token, string name, Guid storageProvider, string connectionString);
+        void UpdateResourceGroup(IResourceGroup item, string name, Guid storageProvider, string connectionString);
+        void DeleteResourceGroup(IResourceGroup item);
 
-		List<IServerResourceGroup> QueryResourceGroups();
-		IServerResourceGroup SelectResourceGroup(Guid token);
-		/*
+        List<IServerResourceGroup> QueryResourceGroups();
+        IServerResourceGroup SelectResourceGroup(Guid token);
+        /*
 		 * Environment variables
 		 */
-		void UpdateEnvironmentVariable(string name, string value);
-		IEnvironmentVariable SelectEnvironmentVariable(string name);
-		List<IEnvironmentVariable> QueryEnvironmentVariables();
-		void DeleteEnvironmentVariable(string name);
-	}
+        void UpdateEnvironmentVariable(string name, string value);
+        IEnvironmentVariable SelectEnvironmentVariable(string name);
+        List<IEnvironmentVariable> QueryEnvironmentVariables();
+        void DeleteEnvironmentVariable(string name);
+    }
 }
