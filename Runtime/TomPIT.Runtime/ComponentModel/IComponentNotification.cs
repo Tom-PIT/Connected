c062d5b047e86fa653ea925bcaaf351616321a72
Matija Koželj <matija.kozelj@tompit.net>
2023-09-18 13:43:48 +0200

    Fix component deletion notifications Fix worker not running due to id mixup

--- Runtime/TomPIT.Runtime/ComponentModel/IComponentNotification.cs
+++ Runtime/TomPIT.Runtime/ComponentModel/IComponentNotification.cs
@@ -5,6 +5,7 @@
 		void NotifyChanged(object sender, ComponentEventArgs e);
 		void NotifyRemoved(object sender, ComponentEventArgs e);
 		void NotifyAdded(object sender, ComponentEventArgs e);
+		void NotifyDeleting(object sender, ComponentEventArgs e);
 
 		void NotifyChanged(object sender, ConfigurationEventArgs e);
 		void NotifyAdded(object sender, ConfigurationEventArgs e);
