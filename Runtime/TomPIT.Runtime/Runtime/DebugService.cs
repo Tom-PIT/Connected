cbc6769b137faa85f9002dcb70756ee8f99022a5
Tom Pipinic <tom@tompit.net>
2023-10-14 17:51:31 +0200

    Added buffering to DebugService

--- Runtime/TomPIT.Runtime/Runtime/DebugService.cs
+++ Runtime/TomPIT.Runtime/Runtime/DebugService.cs
@@ -1,14 +1,27 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Text;
 using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
 using TomPIT.Connectivity;
 using TomPIT.Middleware;
 
 namespace TomPIT.Runtime;
-internal class DebugService : IDebugService
+internal class DebugService : IDebugService, IDisposable
 {
+	private enum DebugType
+	{
+		ConfigurationAdded = 1,
+		ConfigurationChanged = 2,
+		ConfigurationRemoved = 3,
+		ScriptChanged = 4
+	}
 	public DebugService()
 	{
+		Queue = new();
+		Cancel = new();
+
 		if (Shell.Configuration.RootElement.TryGetProperty("debugTarget", out JsonElement debugTarget))
 		{
 			if (debugTarget.TryGetProperty("url", out JsonElement urlNode))
@@ -16,22 +29,82 @@ internal class DebugService : IDebugService
 
 			if (debugTarget.TryGetProperty("token", out JsonElement tokenNode))
 				AuthenticationToken = Encoding.UTF8.GetString(Convert.FromBase64String(tokenNode.GetString()));
+
+			new Task(OnFlush, Cancel.Token, TaskCreationOptions.LongRunning).Start();
 		}
 	}
 
+	private ConcurrentQueue<DebugDescriptor> Queue { get; }
+	private CancellationTokenSource Cancel { get; }
 	private string Url { get; }
 	private string AuthenticationToken { get; }
 	public bool Enabled => !string.IsNullOrEmpty(Url);
+
+	private void OnFlush()
+	{
+		var token = Cancel.Token;
+
+		while (!token.IsCancellationRequested)
+		{
+			try
+			{
+				while (Queue.TryDequeue(out DebugDescriptor descriptor))
+				{
+					try
+					{
+						switch (descriptor.Type)
+						{
+							case DebugType.ConfigurationAdded:
+								MiddlewareDescriptor.Current.Tenant.Post(CreateUrl("ConfigurationAdded"), new
+								{
+									descriptor.Component
+								}, new HttpRequestArgs().WithBearerCredentials(AuthenticationToken));
+
+								break;
+							case DebugType.ConfigurationChanged:
+								MiddlewareDescriptor.Current.Tenant.Post(CreateUrl("ConfigurationChanged"), new
+								{
+									descriptor.Component
+								}, new HttpRequestArgs().WithBearerCredentials(AuthenticationToken));
+
+								break;
+							case DebugType.ConfigurationRemoved:
+								MiddlewareDescriptor.Current.Tenant.Post(CreateUrl("ConfigurationRemoved"), new
+								{
+									descriptor.Component
+								}, new HttpRequestArgs().WithBearerCredentials(AuthenticationToken));
+								break;
+							case DebugType.ScriptChanged:
+								MiddlewareDescriptor.Current.Tenant.Post(CreateUrl("ScriptChanged"), new
+								{
+									descriptor.MicroService,
+									descriptor.Component,
+									descriptor.Element,
+									descriptor.Token
+								}, new HttpRequestArgs().WithBearerCredentials(AuthenticationToken));
+
+								break;
+						}
+					}
+					catch { }
+				}
+
+				token.WaitHandle.WaitOne(TimeSpan.FromSeconds(1));
+			}
+			catch { }
+		}
+	}
 
 	public void ConfigurationAdded(Guid component)
 	{
 		if (!Enabled)
 			return;
 
-		MiddlewareDescriptor.Current.Tenant.Post(CreateUrl("ConfigurationAdded"), new
+		Queue.Enqueue(new DebugDescriptor
 		{
-			component
-		}, new HttpRequestArgs().WithBearerCredentials(AuthenticationToken));
+			Type = DebugType.ConfigurationAdded,
+			Component = component
+		});
 	}
 
 	public void ConfigurationChanged(Guid component)
@@ -39,10 +112,11 @@ internal class DebugService : IDebugService
 		if (!Enabled)
 			return;
 
-		MiddlewareDescriptor.Current.Tenant.Post(CreateUrl("ConfigurationChanged"), new
+		Queue.Enqueue(new DebugDescriptor
 		{
-			component
-		}, new HttpRequestArgs().WithBearerCredentials(AuthenticationToken));
+			Type = DebugType.ConfigurationChanged,
+			Component = component
+		});
 	}
 
 	public void ConfigurationRemoved(Guid component)
@@ -50,10 +124,11 @@ internal class DebugService : IDebugService
 		if (!Enabled)
 			return;
 
-		MiddlewareDescriptor.Current.Tenant.Post(CreateUrl("ConfigurationRemoved"), new
+		Queue.Enqueue(new DebugDescriptor
 		{
-			component
-		}, new HttpRequestArgs().WithBearerCredentials(AuthenticationToken));
+			Type = DebugType.ConfigurationRemoved,
+			Component = component
+		});
 	}
 
 	public void ScriptChanged(Guid microService, Guid component, Guid element, Guid token)
@@ -61,17 +136,32 @@ internal class DebugService : IDebugService
 		if (!Enabled)
 			return;
 
-		MiddlewareDescriptor.Current.Tenant.Post(CreateUrl("ScriptChanged"), new
+		Queue.Enqueue(new DebugDescriptor
 		{
-			microService,
-			component,
-			element,
-			token
-		}, new HttpRequestArgs().WithBearerCredentials(AuthenticationToken));
+			Component = component,
+			MicroService = microService,
+			Element = element,
+			Token = token,
+			Type = DebugType.ScriptChanged
+		});
 	}
 
 	private string CreateUrl(string action)
 	{
 		return $"{Url}/sys/debug/{action}";
+	}
+
+	public void Dispose()
+	{
+		Cancel.Cancel();
+	}
+
+	private class DebugDescriptor
+	{
+		public DebugType Type { get; set; }
+		public Guid MicroService { get; set; }
+		public Guid Component { get; set; }
+		public Guid Element { get; set; }
+		public Guid Token { get; set; }
 	}
 }
