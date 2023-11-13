5a61f6d2ab343ce1d22a7ab75d18c034b8e74379
Tom Pipinic <tom@tompit.net>
2023-11-13 21:07:31 +0100

    Startup made async

--- TomPIT.Connected/Program.cs
+++ TomPIT.Connected/Program.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
 
 namespace TomPIT.Connected
 {
 	public class Program
 	{
-		public static void Main(string[] args)
+		public static async Task Main(string[] args)
 		{
-			CreateHostBuilder(args).Build().Run();
+			var host = CreateHostBuilder(args).Build();
+
+			foreach (var startup in MicroServices.Startups)
+				await startup.Initialize(host);
+
+			foreach (var startup in MicroServices.Startups)
+				await startup.Start();
+
+			await host.RunAsync();
 		}
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
