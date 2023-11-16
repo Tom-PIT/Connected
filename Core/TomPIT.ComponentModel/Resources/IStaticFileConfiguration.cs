f21710556b4a65dab2380358c2f336ed5376cb63
Tom Pipinic <tom@tompit.net>
2023-11-16 07:11:26 +0100

    Added Extension property to the TextConfiguration

---
+++ Core/TomPIT.ComponentModel/Resources/IStaticFileConfiguration.cs
@@ -0,0 +1,5 @@
+﻿namespace TomPIT.ComponentModel.Resources;
+public interface IStaticFileConfiguration
+{
+	string Extension { get; }
+}
--- Development/TomPIT.Templates.Application/Resources/Text.cs
+++ Development/TomPIT.Templates.Application/Resources/Text.cs
@@ -1,14 +1,30 @@
-﻿using TomPIT.Annotations.Design;
+﻿using System.ComponentModel;
+using TomPIT.Annotations;
+using TomPIT.Annotations.Design;
 using TomPIT.ComponentModel;
 using TomPIT.ComponentModel.Resources;
 
-namespace TomPIT.MicroServices.Resources
+namespace TomPIT.MicroServices.Resources;
+
+[FileNameExtension("txt")]
+[DomDesigner(DomDesignerAttribute.TextDesigner)]
+[Syntax(SyntaxAttribute.Text)]
+public class Text : TextConfiguration, ITextConfiguration, IStaticFileConfiguration
 {
-    [FileNameExtension("txt")]
-    [DomDesigner(DomDesignerAttribute.TextDesigner)]
-    [Syntax(SyntaxAttribute.Text)]
-    public class Text : TextConfiguration, ITextConfiguration
-    {
-        public override string FileName => ToString();
-    }
+	[Browsable(false)]
+	public override string FileName
+	{
+		get
+		{
+			var preferred = ToString();
+
+			if (preferred.Contains('.'))
+				return preferred;
+
+			return $"{preferred}.{Extension}";
+		}
+	}
+
+	[PropertyCategory(PropertyCategoryAttribute.CategoryBehavior)]
+	public string Extension { get; set; } = "txt";
 }
