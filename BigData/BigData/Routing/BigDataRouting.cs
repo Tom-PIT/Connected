48be7d089e4fd905f33d53a7fd468c3ee2208fbe
Matija Koželj <matija.kozelj@tompit.net>
2024-10-11 12:44:11 +0200

    feat: add forced garbage collection to BigData write endpoint

--- BigData/BigData/Routing/BigDataRouting.cs
+++ BigData/BigData/Routing/BigDataRouting.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 using TomPIT.BigData.Controllers;
@@ -18,6 +19,7 @@ namespace TomPIT.BigData.Configuration
 
 				handler.ProcessRequest();
 
+				GC.Collect();
 				return Task.CompletedTask;
 			});
 
