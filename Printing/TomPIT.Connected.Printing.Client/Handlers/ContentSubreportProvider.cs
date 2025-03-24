d236c68a95b932fa43f3bcdf566d885a981abeb7
Matija Koželj <matija.kozelj@tompit.net>
2025-03-24 14:13:09 +0100

    feat: add sync subreport provider

--- Printing/TomPIT.Connected.Printing.Client/Handlers/ContentSubreportProvider.cs
+++ Printing/TomPIT.Connected.Printing.Client/Handlers/ContentSubreportProvider.cs
@@ -19,18 +19,63 @@ using TomPIT.Connected.Printing.Client.Printing;
 
 namespace TomPIT.Connected.Printing.Client.Handlers
 {
-	internal class ContentSubreportProvider : IReportProviderAsync
+	internal class ContentSubreportProviderAsync : IReportProviderAsync
 	{
 		private string _content;
 		Dictionary<string, MemoryStream> _subreports = new Dictionary<string, MemoryStream>();
 
-		public ContentSubreportProvider(string completeContent)
+		public ContentSubreportProviderAsync(string completeContent)
 		{
 			_content = completeContent;
 			_subreports = ParseSubreports(completeContent);
 		}
 
 		public async Task<XtraReport> GetReportAsync(string reportUrl, ReportProviderContext context)
+		{
+			if (_subreports.GetValueOrDefault(reportUrl, null) is not MemoryStream ms)
+				return null;
+
+			try
+			{
+				//ParseXMLHere
+				var report = new XtraReport();
+				report.LoadLayoutFromXml(ms);
+				report.CreateDocument();
+
+				return report;
+			}
+			catch (Exception ex)
+			{
+				Logging.Exception(ex, LoggingLevel.Fatal);
+				throw;
+			}
+		}
+
+		private static Dictionary<string, MemoryStream> ParseSubreports(string content)
+		{
+			var reports = content.Split("<?xml version=\"1.0\" encoding=\"utf-8\"?>", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+			var results = new Dictionary<string, MemoryStream>();
+
+			foreach (var report in reports)
+			{
+				var parsedReport = XmlReader.Create(new StringReader(report));
+				if (!parsedReport.Read())
+					continue;
+
+				var reportName = parsedReport.GetAttribute("Name");
+				results.Add(reportName, new MemoryStream(Encoding.UTF8.GetBytes(report)));
+			}
+
+			return results;
+		}
+	}
+
+	internal class ContentSubreportProvider(string completeContent) : IReportProvider
+	{
+		Dictionary<string, MemoryStream> _subreports = ParseSubreports(completeContent);
+
+		public XtraReport GetReport(string reportUrl, ReportProviderContext context)
 		{
 			if (_subreports.GetValueOrDefault(reportUrl, null) is not MemoryStream ms)
 				return null;
