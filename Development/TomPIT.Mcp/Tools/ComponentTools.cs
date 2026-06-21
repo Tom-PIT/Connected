2147cf82b44aec4ad256f0608c5f91224545cfe1
Matija Koželj <matija.kozelj@tompit.com>
2026-06-22 01:31:57 +0200

    [WIP] feat: fine tune mcp server

--- Development/TomPIT.Mcp/Tools/ComponentTools.cs
+++ Development/TomPIT.Mcp/Tools/ComponentTools.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
--- Development/TomPIT.Mcp/Tools/SearchTools.cs
+++ Development/TomPIT.Mcp/Tools/SearchTools.cs
@@ -1,6 +1,7 @@
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using TomPIT.ComponentModel;
